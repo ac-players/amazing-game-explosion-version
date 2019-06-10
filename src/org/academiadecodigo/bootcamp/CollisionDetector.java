ce8fc74313f87f49473891780f53dbed879b8edd
hugof <hugo.abreu.fernandes@gmail.com>
2019-06-10 20:22:19 +0100

    "Explosion"

---
+++ src/org/academiadecodigo/bootcamp/CollisionDetector.java
@@ -0,0 +1,52 @@
+package org.academiadecodigo.bootcamp;
+
+import org.academiadecodigo.bootcamp.car.Obstacle;
+import org.academiadecodigo.bootcamp.car.Player;
+import org.academiadecodigo.bootcamp.grid.position.GridPosition;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
+public class CollisionDetector {
+
+    private Obstacle[] obstacles;
+    private Player player;
+
+    public CollisionDetector(Obstacle[] obstacles, Player player) {
+
+        this.obstacles = obstacles;
+        this.player = player;
+    }
+
+    public boolean isUnSafe(GridPosition pos) {
+
+        for (Obstacle c : obstacles) {
+
+            if (c.getPos()!= pos && c.getPos().equals(pos)) {
+                return true;
+            }
+
+        }
+
+        return false;
+
+    }
+
+    //Checks for collisions with specific obstacle
+
+    public void check(Obstacle obstacle, Player player) {
+
+        for (Obstacle ic : obstacles) {
+
+            if (ic.getPos().equals(player.getPos())) {
+
+                Picture explosion = new Picture(player.getPos().getCol()*45,player.getPos().getRow()*45,"crash.png");
+
+                explosion.draw();
+
+                player.crash();
+            }
+
+        }
+
+    }
+
+}
